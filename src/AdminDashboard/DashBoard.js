56b3935e77d5b94bd0cc06203fc92fa79e6531e9
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-03-07 10:42:12 +0300

    Added language

--- src/AdminDashboard/DashBoard.js
+++ src/AdminDashboard/DashBoard.js
@@ -5,16 +5,16 @@ import AreaChart from "./AreaChart";
 function DashBoard() {
   return (
     <div className="grid mx-auto   md:grid-cols-2 md:gap-10 gap-4 grid-cols-1">
-      <div className="md:h-[500px]  ">
+      <div className="md:h-[500px] h-[300px]  ">
         <PieChart />
       </div>
-      <div className="md:h-[500px]  ">
+      <div className="md:h-[500px] h-[300px]  ">
         <LineChart />
       </div>
-      <div className="md:h-[500px]  ">
+      <div className="md:h-[500px] h-[300px]  ">
         <AreaChart />
       </div>
-      <div className="md:h-[500px]  ">
+      <div className="md:h-[500px] h-[300px]  ">
         <VerticalBar />
       </div>
     </div>
